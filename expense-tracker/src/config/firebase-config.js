74360fe708260a0fb9695d6649a55af3f54fd843
nowfuture011130 <105948127+nowfuture011130@users.noreply.github.com>
2024-01-28 15:12:30 -0500

    update

---
+++ expense-tracker/src/config/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATzJNurAREYb1lGRvU0jGd8rjcnnMhszY",
+  authDomain: "expense-tracker-a6f87.firebaseapp.com",
+  projectId: "expense-tracker-a6f87",
+  storageBucket: "expense-tracker-a6f87.appspot.com",
+  messagingSenderId: "262969371513",
+  appId: "1:262969371513:web:633d15f22cb815abc5dda8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
---
+++ expense-tracker/src/pages/expense-tracker/index.jsx
@@ -0,0 +1,3 @@
+export const ExpenseTracker = () => {
+  return <div>ExpenseTracker</div>;
+};
