6f1df351d46fdb150f01966a412830af1719067c
nowfuture011130 <105948127+nowfuture011130@users.noreply.github.com>
2024-01-29 14:51:47 -0500

    update

---
+++ expense-tracker/src/hooks/useAddTransactions.js
@@ -0,0 +1,21 @@
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../config/firebase-config";
+import { useGetUserInfo } from "./useGetUserInfo";
+export const useAddTransactions = () => {
+  const transactionCollectionRef = collection(db, "transactions");
+  const { userID } = useGetUserInfo();
+  const addTransactions = async ({
+    description,
+    transactionAmount,
+    transactionType,
+  }) => {
+    await addDoc(transactionCollectionRef, {
+      userID,
+      description,
+      transactionAmount,
+      transactionType,
+      createdAt: serverTimestamp(),
+    });
+  };
+  return { addTransactions };
+};
--- expense-tracker/src/pages/auth/index.jsx
+++ expense-tracker/src/pages/auth/index.jsx
@@ -1,8 +1,11 @@
 import { auth, provider } from "../../config/firebase-config";
 import { signInWithPopup } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
+import { useGetUserInfo } from "../../hooks/useGetUserInfo";
+import "./styles.css";
 export const Auth = () => {
   const navigate = useNavigate();
+  const { isAuth } = useGetUserInfo();
   const signInWithGoogle = async () => {
     const results = await signInWithPopup(auth, provider);
     const authInfo = {
@@ -14,10 +17,15 @@ export const Auth = () => {
     localStorage.setItem("auth", JSON.stringify(authInfo));
     navigate("/expense-tracker");
   };
+
+  if (isAuth) {
+    return <Navigate to={"/expense-tracker"} />;
+  }
+
   return (
     <div className="login-page">
       <p>Sign In With Google To Continue</p>
-      <button className="login-with-google-bin" onClick={signInWithGoogle}>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
         Sign In With Google
       </button>
     </div>
